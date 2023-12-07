4a9db3186015d39ff2cacfa2ba0499b59491caae
Jannik Stebani <jannik.stebani@physik.uni-wuerzburg.de>
2023-12-07 14:42:01 +0100

    Add logging for loglevel deduction from env

--- src/logtools.py
+++ src/logtools.py
@@ -9,11 +9,16 @@ import logging
 DEFAULT_FORMAT: str = '%(asctime)s - %(name)s | %(levelname)s : %(message)s'
 DEFAULT_LOGLEVEL: int = logging.WARNING
 
+logger_name = '.'.join(('main', __file__))
+logger = logging.getLogger(logger_name)
+
 def deduce_loglevel() -> int:
     """Deduce logging level from environment variables."""
     value = os.environ.get('LOGLEVEL', default=None)
     if value:
         loglevel = logging.getLevelName(value.upper())
+        logger.debug(f'Deduced loglevel {loglevel} from raw environment variable: \'{value}\'')
     else:
         loglevel = DEFAULT_LOGLEVEL
+        logger.debug(f'No loglevel spec found in environment. Using default: \'{DEFAULT_LOGLEVEL}\'')
     return loglevel
